5f8e2c9b906bc2e88738b1108f9628b9e35994c1
ste23droid <stefano.brandoli23@gmail.com>
2018-12-18 15:39:28 +0100

    Working neural transfer using open cv

--- openwhisk/__main__.py
+++ openwhisk/__main__.py
@@ -1,100 +1,75 @@
-import json
-import tensorflow as tf
-import numpy as np
+import os
+from cv2 import dnn
+import cv2
 from PIL import Image
 import time
 import base64
 import io
-
-
-def load_graph(frozen_graph_filename):
-    # read protobuf pb into graph_def
-    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-
-    # import the graph_def into a new Graph
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(graph_def, name="")
-
-    return graph
-
-
-tf_nn_num_styles = 26
-image_sizes = [600, 700, 800]
-# 1024 richiede 512
-
-'''
-bike.jpg
-bird.jpg
-boat.jpg
-chicago.jpg
-gate.jpg
-stata.jpg
-tower.jpg
-'''
-
+import json
+import numpy as np
 
 def response_message(statusCode, jsonContent):
     return {"statusCode": statusCode,
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps(jsonContent)}
 
-
 def main(args):
-    # payload = json.loads(event["body"])
-
+    # payload = json.loads(args["body"])
     # image = Image.open(io.BytesIO(base64.b64decode(payload["image"])))
-    image = Image.open("./testImages/bird.jpg")
-    # image.show()
+    img = cv2.imread("./testImages/chicago.jpg")
+    # style = payload["style"]
+    style = 0
 
-    # selected_size = payload["size"]
-    selected_size = 700
-    # selected_style = payload["style"]
-    selected_style = 19
-
-    graph = load_graph("./neuralNetwork/stylize_quantized.pb")
-    input_image_tensor = tf.Tensor(graph.get_operation_by_name('input'), 0, tf.float32)
-    input_image_tensor.set_shape([1, selected_size, selected_size, 3])
-    input_style_tensor = tf.Tensor(graph.get_operation_by_name('style_num'), 0, tf.float32)
-    input_style_tensor.set_shape([tf_nn_num_styles])
-    output_tensor = graph.get_tensor_by_name("transformer/expand/conv3/conv/Sigmoid:0")
-
-    style_vals = np.empty(shape=tf_nn_num_styles, dtype=np.float32)
-    style_vals.fill(0)
-    if selected_style in range(0, tf_nn_num_styles):
-        style_vals[selected_style] = 1;
+    # get models
+    model_path = './models/instance_norm/'
+    models = []
+    for f in sorted(os.listdir(model_path)):
+        if f.endswith('.t7'):
+            models.append(f)
+    
+    # load user chosen model
+    model_loaded_i = 0
+    if style in range(0, len(models)):
+       model_loaded_i = style
+    model_to_load = model_path  + models[model_loaded_i]
+    net = dnn.readNetFromTorch(model_to_load)
+    
+    # resize image if necessary
+    h, w = img.shape[:2]
+    width = 700
+    if w <= width:
+        width = w
+        dim = (w, h)
     else:
-        # select Van Gogh as fallback
-        style_vals[19] = 1;
+        ratio = width / float(w)
+        dim = (width, int(h * ratio))
+        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
-    original_size = image.size
-    transformed_size = selected_size, selected_size
-    image = image.resize(transformed_size)
+    
+    # Feed the neural net
+    blob = dnn.blobFromImage(img, 1.0, dim,
+        (103.939, 116.779, 123.680), swapRB=False, crop=False)
+    net.setInput(blob)
 
-    image = np.array(image, dtype=np.float32)
-    image = np.expand_dims(image, axis=0)
+    # Run neural network
+    out = net.forward()
 
-    start_millis = int(round(time.time() * 1000))
-    with tf.Session(graph=graph) as sess:
+    # Add back in the mean subtraction and then swap the channel ordering
+    out = out.reshape((3, out.shape[2], out.shape[3]))
+    out[0] += 103.939
+    out[1] += 116.779
+    out[2] += 123.680
+    out = out.transpose(1,2,0)
 
-        output = sess.run(output_tensor,
-                          feed_dict={input_image_tensor: image,
-                                     input_style_tensor: style_vals
-                                     }
-                          )
-        output_squeezed = np.array(np.squeeze(output, axis=0) * 255, dtype=np.uint8)
-        image = Image.fromarray(output_squeezed).resize(original_size)
-        # image = Image.fromarray(output_squeezed)
-        # image.show()
-        print("Image stylized successfully !!!")
-        millis = int(round(time.time() * 1000)) - start_millis
-        print("TF Computation time: {} milliseconds".format(millis))
+    # convert image from BGR to RGB and then to uint8 without strange artifacts
+    out = cv2.convertScaleAbs(cv2.cvtColor(out, cv2.COLOR_BGR2RGB))
+    img = Image.fromarray(out)
 
-        result_buffer = io.BytesIO()
-        image.save(result_buffer, format='PNG')
-        encoded_result = base64.b64encode(result_buffer.getvalue())
-        return response_message(200, {'image': encoded_result})
+    # base 64 encode response
+    result_buffer = io.BytesIO()
+    img.save(result_buffer, format='PNG')
+    img = base64.b64encode(result_buffer.getvalue())
+    return response_message(200, {'stylizedImage': img})
 
 # if __name__ == '__main__':
 #    main({})
