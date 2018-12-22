6d5d371fdfd21e4e5bb4630fa2abeae1fa7170db
ste23droid <stefano.brandoli23@gmail.com>
2018-12-22 13:42:47 +0100

    Modified code

--- openwhisk/__main__.py
+++ openwhisk/__main__.py
@@ -1,39 +1,27 @@
-import os
-from cv2 import dnn
 import cv2
-from PIL import Image
-import time
+# from PIL import Image
+# import time
 import base64
-import io
-import json
+# import io
+# import json
 import numpy as np
 
-def response_message(statusCode, jsonContent):
-    return {"statusCode": statusCode,
-            "headers": {"Content-Type": "application/json"},
-            "body": json.dumps(jsonContent)}
-
 def main(args):
-    # payload = json.loads(args["body"])
-    # image = Image.open(io.BytesIO(base64.b64decode(payload["image"])))
-    img = cv2.imread("./testImages/chicago.jpg")
-    # style = payload["style"]
-    style = 0
-
     # get models
-    model_path = './models/instance_norm/'
-    models = []
-    for f in sorted(os.listdir(model_path)):
-        if f.endswith('.t7'):
-            models.append(f)
+    models = ["./models/instance_norm/udnie.t7",
+              "./models/instance_norm/mosaic.t7",
+              "./models/instance_norm/candy.t7" ]
+
+    #img = cv2.imread("./testImages/chicago.jpg")
+    #style = 0
     
+    # >0 Return a 3-channel color image.
+    img = cv2.imdecode(np.fromstring(base64.b64decode(args["image"]), dtype=np.uint8),1)
+    style = args["style"]
+
     # load user chosen model
-    model_loaded_i = 0
-    if style in range(0, len(models)):
-       model_loaded_i = style
-    model_to_load = model_path  + models[model_loaded_i]
-    net = dnn.readNetFromTorch(model_to_load)
-    
+    net = cv2.dnn.readNetFromTorch(models[style])
+
     # resize image if necessary
     h, w = img.shape[:2]
     width = 700
@@ -45,9 +33,8 @@ def main(args):
         dim = (width, int(h * ratio))
         img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
-    
     # Feed the neural net
-    blob = dnn.blobFromImage(img, 1.0, dim,
+    blob = cv2.dnn.blobFromImage(img, 1.0, dim,
         (103.939, 116.779, 123.680), swapRB=False, crop=False)
     net.setInput(blob)
 
@@ -63,13 +50,10 @@ def main(args):
 
     # convert image from BGR to RGB and then to uint8 without strange artifacts
     out = cv2.convertScaleAbs(cv2.cvtColor(out, cv2.COLOR_BGR2RGB))
-    img = Image.fromarray(out)
-
-    # base 64 encode response
-    result_buffer = io.BytesIO()
-    img.save(result_buffer, format='PNG')
-    img = base64.b64encode(result_buffer.getvalue())
-    return response_message(200, {'stylizedImage': img})
+    
+    # https://docs.opencv.org/3.0-beta/modules/imgcodecs/doc/reading_and_writing_images.html#imencode 
+    retval, buffer = cv2.imencode('.png', out)
+    return {"statusCode":200,"headers":{"Content-Type":"application/json"},"body":{"stylizedImage":base64.b64encode(buffer)}}
 
 # if __name__ == '__main__':
 #    main({})
